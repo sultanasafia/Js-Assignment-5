eaa4e5c2e3502c6e4276780f02101dd7b60857ec
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2023-11-02 16:51:50 +0500

    js=ass-5

---
+++ app.js
@@ -0,0 +1,116 @@
+// Q1==============
+// let a = 3;
+// let b = 5;
+// document.write('Sum of 3 and 5 is '+ (a+b))
+// Q2==============
+// let a = 3;
+// let b = 5;
+// document.write('Sub of 3 and 5 is '+ (a-b))
+// Q2(b)==================
+// let a = 3;
+// let b = 5;
+// document.write('Mul of 3 and 5 is '+ (a*b))
+// Q2(c)==================
+// let a = 3;
+// let b = 5;
+// document.write('Division of 3 and 5 is '+ (a/b))
+// Q2(d)==================
+// let a = 3;
+// let b = 5;
+// document.write('Division of 3 and 5 is '+ (a/b))
+// Q2(e)===================
+// let a = 3;
+// let b = 5;
+// document.write('Modulos of 3 and 5 is '+ (a%b))
+// Q3=================
+// let num ;
+// document.write("Value after variable declaration is:" +num)
+// document.write("<br>")
+// // (b)
+// num = 5
+// document.write("Initial value is: "+ num)
+// document.write("<br>")
+// ++num
+
+// document.write('Value after Increment: '+ num)
+// document.write("<br>")
+// document.write('Value after addition is: '+ (7 + num))
+// let num2 = 7 + num
+// console.log(num2);
+// --num2
+// document.write("<br>")
+// document.write('Value after decrement is: '+ (num2))
+// document.write("<br>")
+// document.write('The remainder value is: '+ (3 / num2 ))
+
+// Q4============================
+// let movieTicket = 600;
+// let costOfBuying = 5;
+// let answer = movieTicket * costOfBuying
+// document.write("Total cost to buy 5 tickets to a movie is " + answer+ "PKR")
+// Q5===============
+// let table = prompt("Enter your table");
+// for (let i = 1 ; i <= 10 ; i++){
+//     document.write(table + " x " + i + " = " +table*i+ "<br>")
+// }
+// Q6==================
+// var celsiusTemp = 30; 
+// var fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+// console.log(celsiusTemp + "°C is " + fahrenheitTemp + "°F");
+// var fahrenheitTemp2 = 86; 
+// var celsiusTemp2 = (fahrenheitTemp2 - 32) * 5/9;
+// console.log(fahrenheitTemp2 + "°F is " + celsiusTemp2 + "°C");
+// Q7==============
+// document.write('<h1>shopping cart</h1> ')
+// let priceOfItem1  = 650;
+// let priceOfItem2  = 100;
+// let quantityOfItem1  = 3;
+// let quantityOfItem2  = 7;
+// let shippinCharges = 100;
+// let p1 = priceOfItem1* quantityOfItem1
+// let p2 = priceOfItem2* quantityOfItem2
+// let add = p1+p2+shippinCharges
+// document.write("Total of your order cost is " + add)
+// Q8==============
+// let totalM = 980;
+// let marksObt = 804;
+// let answer = marksObt / totalM * 100;
+// document.write("percentage is "+answer)
+// Q9======================
+// let  usDollar =  104.80;
+// let saudiRiyal =  28;
+// let usDollarQuant = 10;
+// let saudiRiyalQuant = 25;
+// let quanAnsDollar = usDollar* usDollarQuant
+// let quanAnsRiyal = saudiRiyal* saudiRiyalQuant
+// let rezult = quanAnsDollar+ quanAnsRiyal
+// document.write(" total currency in pkr :"+rezult)
+
+
+
+// Q10================
+// let add = ((5 + 5)*10)/2
+// document.write(add)
+// Q11===============
+// let date = new Date()
+// let cuYear = date.getFullYear()
+// let myDate = new Date("9 Oct, 1991")
+// let myYear = myDate.getFullYear()
+// let sub = cuYear - myYear
+// console.log(`Current year is ${cuYear}\n BirthYear is ${myYear} \n  Your current age is ${sub}`);
+// Q12======================
+
+// var radius = 5; 
+// var circumference = 2 * Math.PI * radius;
+// var area = Math.PI * Math.pow(radius, 2);
+// console.log("The circumference is " + circumference);
+// console.log("The area is " + area);
+// Q13=================
+// let favSnack = "Chocolatechip";
+// let age = 15;
+// let estimateAge = 65-age;
+// console.log(estimateAge);
+// let amountOfpDay = estimateAge * 3;
+// console.log(amountOfpDay);
+// document.write(`You will need ${amountOfpDay} chocolatechip to last you untill the ripe ole age of 65`)
+// Assignment completed===========================================
